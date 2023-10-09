05c68d2b83561387693cc32fb57b244e8ef3f4a4
heejae <moon49051130@gmail.com>
2023-10-10 03:08:06 +0900

    spring boot로 프로젝트 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -4,30 +4,37 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
-import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 
 // 생성 + 주입
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
 
         // 생성자 주입
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
 
         // 생성자 주입
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private FixDiscountPolicy discountPolicy() {
+    @Bean
+    public FixDiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
 
--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -2,13 +2,21 @@ package hello.core.member;
 
 import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    AppConfig appConfig = new AppConfig();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+
+        AppConfig appConfig = new AppConfig();
 //    MemberService memberService = new MemberServiceImpl();
-    MemberService memberService = appConfig.memberService();
+        memberService = appConfig.memberService();
+
+    }
 
     @Test
     void join(){
